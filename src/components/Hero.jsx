689bf3053462e9177d9ff68978971fc9182cd70b
Yhucee14 <uchechukwu7814@gmail.com>
2024-11-16 10:28:46 -0800

    correction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,23 +4,23 @@ import phone from "../assets/phone.png";
 const Hero = () => {
   return (
     <div>
-      <div className="flex xx:py-4 sm:py-2 xx:flex-col sm:flex-col justify-between items-center">
+      <div className="flex xx:py-4 xx:px-14 sm:px-0 sm:py-2 xx:flex-col sm:flex-col justify-between items-center">
         <div className="flex px-4 flex-col">
-          <div className="font-bold flex flex-col justify-start xx:leading-10 sm:leading-10 sm:py-3 text-2xl sm:text-3xl  text-[#0F2816]">
+          <div className="font-bold flex flex-col xx:px-2 sm:px-0 justify-start xx:leading-10 sm:leading-10 sm:py-3 text-2xl sm:text-3xl  text-[#0F2816]">
             <p className="flex justify-start px-36 sm:px-0  sm:justify-center ">Empowering Sustainable</p>
             <p className="flex justify-start xx:leading-9 sm:leading-0 xx:px-36  sm:px-0">
               
               Agriculture through Decentralized Investing.
             </p>
           </div>
-          <div className=" py-4 flex xx:justify-start sm:justify-center w-[650px] sm:px-5 font-normal leading-6 xx:text-md sm:text-lg">
+          <div className=" py-4 flex xx:justify-start sm:justify-center w-[650px] px-2 sm:px-5 font-normal leading-6 xx:text-md sm:text-lg">
             <p className="text-[#000000ad] flex xx:px-36 xx:leading-5 sm:leading-6 sm:px-0 justify-start sm:justify-center sm:text-center">
               Be the first to contribute to sustainable agriculture on CropCrypt
               and also get insights about our launch{" "}
             </p>
           </div>
           <div className="flex py-2 gap-2 px-36 sm:px-0 justify-start sm:justify-center flex-row">
-            <div className="flex py-1 justify-center sm:px-5 items-center">
+            <div className="flex py-1 justify-center px-2 sm:px-5 items-center">
               <Link to="/waitlist">
                 <button className="flex bg-gradient-green w-full hover:bg-none hover:bg-white hover:text-[#1B6909] hover:border-[#1B6909] border border-[#1B6909] rounded-3xl py-1.5 sm:py-2 sm:px-2 text-white justify-center items-center transition-all duration-300 ease-in-out">
                   <h1 className="px-3">Join the Waitlist</h1>
@@ -37,12 +37,12 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="xx:pb-16 sm:pb-0 lg:pb-10">
+        <div className="xx:pb-16 xx:pt-8 sm:pt-0 sm:pb-0 lg:pb-10">
           <img src={phone} alt="home" className=" z-20" />
         </div>
       </div>
 
-      <div className="flex justify-center font-normal text-sm text-black">
+      <div className="flex justify-center xx:pt-24 sm:pt-20  lg:pt-20px font-normal text-sm text-black">
         @2024-2025 Cropcrypt. All rights reserved.
       </div>
     </div>
--- src/routes/Community.jsx
+++ src/routes/Community.jsx
@@ -15,7 +15,7 @@ const Community = () => {
         style={{ backgroundImage: `url(${herobg})` }}
       >
         <div className="flex justify-center xx:pb-16 sm:pb-32 lg:pb-40 xx:mt-[20px] sm:mt-0 lg:mt-[32px] py-3">
-          <div className="border w-[400px] backdrop-blur-lg sm:w-[600px] sm:mt-[10px] rounded-3xl bg-[#D9D9D94D] flex flex-col py-5 sm:px-10 sm:justify-center sm:items-center">
+          <div className="border w-[400px] xx:mt-12 sm:pt-0 backdrop-blur-lg sm:w-[600px] sm:mt-[10px] rounded-3xl bg-[#D9D9D94D] flex flex-col py-5 sm:px-10 sm:justify-center sm:items-center">
             <div className="font-bold flex justify-center text-xl">
               Join our Community
             </div>
@@ -27,7 +27,7 @@ const Community = () => {
               </p>
             </div>
 
-            <div className="py-5 flex flex-col justify-center">
+            <div className="py-5 flex xx:pt-8 sm:pt-0 xx:pb-16 sm:pb-0 flex-col justify-center">
               <a href="https://www.x.com" target="_blank" className="flex justify-center">
                 <div className="flex flex-col justify-center py-1 w-[350px]">
                   <button
@@ -89,7 +89,7 @@ const Community = () => {
           </div>
         </div>
 
-        <div className="flex justify-center xx:pt-24 sm:pt-20  lg:pt-20px font-normal text-sm text-black">
+        <div className="flex justify-center xx:mt-28 sm:mt-20  lg:mt-20px font-normal text-sm text-black">
           @2024-2025 Cropcrypt. All rights reserved.
         </div>
       </div>
