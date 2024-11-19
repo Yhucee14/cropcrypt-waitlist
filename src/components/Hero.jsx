8ba056d6eac602bb5625458edd02a7ee979489f4
Yhucee14 <uchechukwu7814@gmail.com>
2024-11-19 07:55:23 -0800

    more changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,14 @@ import Footer from "./Footer";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col justify-between xx:gap-[60px] xs:gap-[160px] ss:gap-[130px] md:gap-[2px] lg:gap-25px]">
-      <div className="flex py-4 px-4 sm:px-0 flex-col justify-center ss:items-center">
+    <div className="flex flex-col justify-between xx:gap-[60px] xs:gap-[160px] ss:gap-[130px] md:gap-[2px] lg:gap-[0px]">
+      <div className="flex py-0 px-4 sm:px-0 flex-col justify-center ss:items-center">
         <div className="flex flex-col justify-center ss:items-center ">
           <div className="font-bold px-2 sm:px-0 leading-8 sm:leading-10 sm:py-3 ss:text-center text-2xl sm:text-3xl text-[#0F2816] max-w-full">
             <p>Empowering Sustainable</p>
             <p>Agriculture through Decentralized Investing.</p>
           </div>
-          <div className="py-3 px-2 sm:px-5 sm:w-[650px] ss:text-center font-normal text-md sm:text-lg text-[#000000ad]  leading-5 sm:leading-5">
+          <div className="py-3 md:py-1 px-2 sm:px-5 sm:w-[650px] ss:text-center font-normal text-md sm:text-lg text-[#000000ad]  leading-5 sm:leading-5">
             <p>
               Be the first to contribute to sustainable agriculture on CropCrypt
               and also get insights about our launch.
@@ -34,8 +34,8 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="xx:mt-[60px] sm:mt-[10px] lg:mt-[30px] lg:pb-[35px]">
-          <img src={phone} alt="home" className="z-20" />
+        <div className="xx:mt-[40px] sm:mt-[0px] md:mt-[0px] lg:mt-[10px] lg:pb-[25px]">
+          <img src={phone} alt="home" className="z-20 w-[500px]" />
         </div>
       </div>
       <Footer />
