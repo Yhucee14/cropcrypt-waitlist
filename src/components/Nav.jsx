893b92430895523b31af0d4a1ecf3e0c7cedb87f
Yhucee14 <uchechukwu7814@gmail.com>
2024-11-20 06:50:57 -0800

    today

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,18 @@
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import home from "../assets/home.png";
 import logo from "../assets/logo.png";
 import menu from "../assets/menu.png";
 import close from "../assets/close.png";
 import people from "../assets/people.png";
 import { Link, useLocation } from "react-router-dom";
-import { motion } from "framer-motion";
 
 const Nav = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 1023);
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const location = useLocation();
-  const isActive = (path) =>
-    location.pathname === path ? "text-green-600" : "text-black";
+  const isActive = (path) => location.pathname === path;
 
   useEffect(() => {
     const handleResize = () => {
@@ -47,14 +46,13 @@ const Nav = () => {
     },
   };
 
-
   return (
     <div className="flex px-0 sm:px-0 py-4 justify-between cursor-pointer">
       {isMobile ? (
         <div className="md:hidden shadow-md py-2 z-50 flex justify-between w-full items-center">
-          <div className="py-1 px-4 flex flex-row ">
-            <Link to="/" className="flex ">
-              <img src={logo} alt="logo" className=" w-10 h-7" />
+          <div className="py-1 px-4 flex flex-row">
+            <Link to="/" className="flex">
+              <img src={logo} alt="logo" className="w-10 h-7" />
               <div className="px-2 font-bold text-lg bg-gradient-to-r from-black to-[#359A35] text-transparent bg-clip-text flex justify-center items-center">
                 Cropcrypt
               </div>
@@ -68,36 +66,51 @@ const Nav = () => {
               className="h-6 w-6"
             />
           </button>
+
           {/* Mobile Menu */}
           {isMobileMenuOpen && (
-            <motion.div className="absolute top-20 left-0  w-full rounded-lg bg-white shadow-lg md:hidden z-10"
-            initial="closed"
-            animate={isMobileMenuOpen ? "open" : "closed"}
-            variants={menuVariants} >
+            <motion.div
+              className="absolute top-20 left-0 w-full rounded-lg bg-white shadow-lg md:hidden z-10"
+              initial="closed"
+              animate={isMobileMenuOpen ? "open" : "closed"}
+              variants={menuVariants}
+            >
               <div className="py-2">
                 <Link
                   to="/"
-                  className={`flex border-b-gray-300 border-b-2 px-5 md:px-2 py-3 font-medium lg:px-2 ${isActive(
-                    "/"
-                  )}`}
+                  className={`flex items-center border-b-gray-300 border-b-2 px-5 md:px-2 py-3 font-medium lg:px-2 ${
+                    isActive("/") ? "text-green-600" : "text-black"
+                  }`}
                 >
                   <div className="flex justify-center items-center">
-                    <img src={home} alt="home" className="w-5 h-5" />
+                    <img
+                      src={home}
+                      alt="home"
+                      className={`w-5 h-5 ${
+                        isActive("/") ? "filter-green" : ""
+                      }`}
+                    />
                   </div>
-                  <div className="px-2  flex justify-center items-center">
+                  <div className="px-2 flex justify-center items-center">
                     Home
                   </div>
                 </Link>
                 <Link
                   to="/community"
-                  className={`flex px-5 border-b-gray-300 border-b-2 py-3 md:px-2 font-medium lg:px-2 ${isActive(
-                    "/community"
-                  )}`}
+                  className={`flex items-center px-5 border-b-gray-300 border-b-2 py-3 md:px-2 font-medium lg:px-2 ${
+                    isActive("/community") ? "text-green-600" : "text-black"
+                  }`}
                 >
                   <div className="flex justify-center items-center">
-                    <img src={people} alt="home" className="w-5 h-5" />
+                    <img
+                      src={people}
+                      alt="community"
+                      className={`w-5 h-5 ${
+                        isActive("/community") ? "filter-green" : ""
+                      }`}
+                    />
                   </div>
-                  <div className="px-2  flex justify-center items-center">
+                  <div className="px-2 flex justify-center items-center">
                     Community
                   </div>
                 </Link>
@@ -115,8 +128,8 @@ const Nav = () => {
         </div>
       ) : (
         <div className="flex flex-row px-4 justify-between w-screen">
-          <div className=" flex flex-row ">
-            <Link to="/" className="flex ">
+          <div className="flex flex-row">
+            <Link to="/" className="flex">
               <img src={logo} alt="logo" className="w-14 h-11" />
               <div className="px-2 font-bold text-xl bg-gradient-to-r from-black to-[#359A35] text-transparent bg-clip-text flex justify-center items-center">
                 Cropcrypt
@@ -127,10 +140,16 @@ const Nav = () => {
           <div className="flex flex-row justify-center md:gap-2 lg:gap-8">
             <Link
               to="/"
-              className={`flex md:px-2 font-medium lg:px-2 ${isActive("/")}`}
+              className={`flex items-center md:px-2 font-medium lg:px-2 ${
+                isActive("/") ? "text-green-600" : "text-black"
+              }`}
             >
               <div className="flex justify-center items-center">
-                <img src={home} alt="home" className="w-5 h-5" />
+                <img
+                  src={home}
+                  alt="home"
+                  className={`w-5 h-5 ${isActive("/") ? "filter-green" : ""}`}
+                />
               </div>
               <div className="px-2 mt-0.5 flex justify-center items-center">
                 Home
@@ -139,12 +158,18 @@ const Nav = () => {
 
             <Link
               to="/community"
-              className={`flex md:px-1 font-medium lg:px-2 ${isActive(
-                "/community"
-              )}`}
+              className={`flex items-center md:px-1 font-medium lg:px-2 ${
+                isActive("/community") ? "text-green-600" : "text-black"
+              }`}
             >
               <div className="flex justify-center items-center">
-                <img src={people} alt="profile" className="w-5 h-5" />
+                <img
+                  src={people}
+                  alt="community"
+                  className={`w-5 h-5 ${
+                    isActive("/community") ? "filter-green" : ""
+                  }`}
+                />
               </div>
               <div className="px-2 mt-0.5 flex justify-center items-center">
                 Community
